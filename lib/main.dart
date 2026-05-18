e0690c0de3747267a90501263905fef38404856c
Hanya Essam <hanyaessam2050@gmail.com>
2026-05-19 00:49:35 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,11 @@ import 'package:news/ui/home/screens/home_screen.dart';
 import 'package:news/ui/search/search_screen.dart';
 import 'package:news/ui/save/save_screen.dart';
 import 'package:news/ui/profile/profile_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
